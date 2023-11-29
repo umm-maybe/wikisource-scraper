32d3fe779863a53380b95b30ac5c16fc44d3a6af
umm-maybe <colby@manhangroup.com>
2023-11-28 23:36:31 -0500

    Sensible defaults

--- wikisource-scraper.py
+++ wikisource-scraper.py
@@ -6,6 +6,7 @@ import logging
 import requests
 from bs4 import BeautifulSoup, UnicodeDammit
 from wikitextparser import remove_markup, parse
+import os
 
 logLevel = logging.INFO
 
@@ -35,25 +36,47 @@ def convert_from_url(url: str):
         return None
     
     document = BeautifulSoup(res.text, features='lxml')
+    title = document.find('title').string
+    if title:
+        print(title)
+    else:
+        print("No title found")
+        return None
     text_area = document.body.find('div', id='mw-content-text')
     output_text = text_area.select('div p')
-    text = ""
+    text = 'Title: "' + title.split(" - Wikisource")[0] + '"\n\n'
+    skip = ["talk page"]
+    stop = ["public domain"]
     for p in output_text :
-        text += parse(str(p)).plain_text()
-
-    filename_base = match.group(1).strip().replace('/', '-')
-    filename = filename_base + ".txt"
-    with open(filename, "w") as f:
+        if any(x in p.get_text().lower() for x in stop):
+            break
+        elif any(x in p.get_text().lower() for x in skip):
+            continue
+        text += p.get_text()
+    if not text:
+        print("No text found")
+        return None
+    filename = title.replace(" ","_") + ".txt"
+    with open(os.path.join("outputs",filename), "w") as f:
         f.write(text)
     return filename
 
 if __name__ == '__main__':
     urls = sys.argv[1:]
+    if len(urls) == 0:
+        while True:
+            log.info("No urls provided; pulling random page from wikisource")
+            result = convert_from_url("https://en.wikisource.org/wiki/Special:RandomRootpage/Main")
+            if result:
+                log.info("Converted random page to text file: " + result)
+                break
+            else:
+                log.warning("Conversion failed for random page")
     for url in urls:
         log.info("Starting conversion for: " + url)
         result = convert_from_url(url)
         if result:
             log.info("Converted " + url + " to text file: " + result)
-        else:
-            log.warning("Conversion failed for " + url)
+    if not result:
+        log.warning("Conversion failed for " + url)
     sys.exit(0)
